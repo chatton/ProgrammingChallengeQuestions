442dd66ba25fb1ce0d4a7e598a3bdcdb5ff8136c
chatton <cianhatton@gmail.com>
2017-10-26 22:25:31 +0100

    added challenge description and more comments

--- change-return.py
+++ change-return.py
@@ -1,3 +1,11 @@
+"""
+Write a function turn_into_coins that will take
+an amount of cash as an integer and return a map
+of value -> no. of coins. Your function should return
+the minimum requred coins and their denominations needed
+to break up that amount of cash into change.
+"""
+
 
 num_coins = [1,2,5,10,20,50,100,200]
 
@@ -11,9 +19,9 @@ def turn_into_coins(total_cash):
     denominations = sorted(num_coins, reverse=True) # biggest values first.
     for num in denominations: # looking at every denomination of coin.
         while num <= total_cash: # look at this denomination until it's too big
-            total_cash -= num
-            val = values.get(num, 0)
-            val += 1
+            total_cash -= num # taking this amount away from the total cash
+            val = values.get(num, 0) # start at 0 if we haven't had any of this type yet
+            val += 1 # we now have one more
             values[num] = val
     return values
 
