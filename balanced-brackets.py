3aa771b0288aba48f05bc07a93ca7674eac37430
chatton <cianhatton@gmail.com>
2018-04-05 11:59:51 +0100

    added bracket-balancing solution

---
+++ balanced-brackets.py
@@ -0,0 +1,35 @@
+
+
+def balance_check(s):
+    opening = {
+        "(": 1,
+        "{": 2,
+        "]": 3
+    }
+    closing = {
+        ")": 1,
+        "}": 2,
+        "]": 3
+    }
+
+    open_stack = []
+    for paren in s:
+        if paren in opening:
+            open_stack.append(paren)
+
+        if paren in closing:
+            if not open_stack:  # seeing a closing before an opening, can't be true
+                return False
+
+            # the closing is matching the last opening
+            if closing[paren] != opening[open_stack.pop()]:
+                return False
+
+    return True
+
+
+assert balance_check("{[]}") is True
+assert balance_check("{([])}") is True
+assert balance_check("{([]})") is False
+assert balance_check("{([[{}]])}") is True
+assert balance_check("([])}") is False
---
+++ largest-cont-sum.py
@@ -0,0 +1,4 @@
+def larget_cont_sum(arr):
+    pass
+
+print(len(larget_cont_sum([1,2,-1,3,4,10,10,-10,-1])))
