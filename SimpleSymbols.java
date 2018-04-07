d7cb2ace6ead1e8b5b5463260d1f376519b13f64
chatton <cianhatton@gmail.com>
2018-04-07 18:15:18 +0100

    added solution to SimpleSymbols coderbyte challenge

---
+++ SimpleSymbols.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static String SimpleSymbols(String str) {
+
+        for (int i = 0; i < str.length(); i++) {
+            char letter = str.charAt(i);
+            if (Character.isLetter(letter)) {
+                // check before and after for +
+                if (i == 0 || i == str.length() - 1) {
+                    return "false"; // can't have a + before it if it's the first character or after if it's the last
+                }
+
+                if (str.charAt(i - 1) != '+' || str.charAt(i + 1) != '+') {
+                    return "false"; // the character isn't surrounded by plusses
+                }
+            }
+        }
+        return "true";
+
+    }
+
+}
