40ab8e84aa5ab73a90d3481058d46b93f427bccd
chatton <cianhatton@gmail.com>
2018-04-10 14:24:06 +0100

    added AllUnique solution

---
+++ go/AllUnique.go
@@ -0,0 +1,13 @@
+package kata
+
+func HasUniqueChar (str string) bool {
+  counts := make(map[rune]bool)
+  for _, char := range str {
+    if ok, _ := counts[char]; ok {
+      return false
+    } else {
+      counts[char] = true
+    }
+  }
+  return true
+}
