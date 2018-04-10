61807bffafbbb3c1c931cca1ee3a1e68115214f4
chatton <cianhatton@gmail.com>
2018-04-10 14:46:15 +0100

    added WordSpinning solution

---
+++ csharp/WordSpinning.cs
@@ -0,0 +1,33 @@
+/*
+Description:
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+
+Examples:
+
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+spinWords( "This is a test") => returns "This is a test" 
+spinWords( "This is another test" )=> returns "This is rehtona test"
+
+ */
+
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Text;
+
+public class Kata
+{
+  public static string SpinWords(string sentence)
+  {
+  var words = new List<string>(sentence.Split(null))
+    .Select(s => {
+        if(s.Length >= 5){
+            return new string(s.ToCharArray().Reverse().ToArray());
+        }
+        return s;
+    }).ToList();
+    
+  return String.Join(" ", words);
+  }
+}
