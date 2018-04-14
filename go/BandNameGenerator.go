c3dd8a5695bc176db75fe9f2ae28ca395d865197
chatton <cianhatton@gmail.com>
2018-04-14 16:10:58 +0100

    added band name generator function

---
+++ go/BandNameGenerator.go
@@ -0,0 +1,30 @@
+/*
+
+Description:
+My friend wants a new band name for her band. She like bands that use the formula: "The" + a noun with the first letter capitalized, for example:
+
+"dolphin" -> "The Dolphin"
+
+However, when a noun STARTS and ENDS with the same letter, she likes to repeat the noun twice and connect them together with the first and last letter, combined into one word (WITHOUT "The" in front), like this:
+
+"alaska" -> "Alaskalaska"
+
+Complete the function that takes a noun as a string, and returns her preferred band name written as a string.
+
+*/
+
+package kata
+import (
+  "strings"
+)
+
+func bandNameGenerator(word string) string {
+
+  titledRunes := []rune(strings.Title(word))
+  asLower := []rune(strings.ToLower(word))
+  if needsThe :=  asLower[0] != asLower[len(asLower) - 1]; needsThe {
+    return "The " + string(titledRunes)  
+  }
+
+  return string(titledRunes) + word[1:]
+}
