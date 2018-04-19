c6a5ac17a438e5c09d1181e5a48e47d498bc5ca6
chatton <cianhatton@gmail.com>
2018-04-19 18:51:39 +0100

    added centry from year

---
+++ go/RemoveFirstAndLast.go
@@ -0,0 +1,10 @@
+/*
+Description:
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+*/
+package kata
+
+func RemoveChar(word string) string {
+  runes := []rune(word)
+  return string(runes[1: len(runes) - 1])
+}
